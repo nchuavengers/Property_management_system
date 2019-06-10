55caa1c9e2eede1a1605e11a7fec3c1daa05ff24
S16202132 <2325740955@qq.com>
2019-06-10 13:04:52 +0800

    change dependence to mybit bootstrap 4.3

--- EyesOfGod/src/main/java/pms/repository/TeacherRepository.java
+++ EyesOfGod/src/main/java/pms/repository/TeacherRepository.java
@@ -3,37 +3,58 @@ package pms.repository;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.type.JdbcType;
 
 import pms.entity.*;
 
-@Repository
-public class TeacherRepository {	
-	@Autowired
-	private JdbcTemplate jdbcTemplate;	
-	public List<Teacher> findAll(){		
-		String sql="select teacher_id as id,teacher_account as account,teacher_name as name from t_teacher ";		
-		RowMapper<Teacher> rowMapper=new BeanPropertyRowMapper<Teacher>(Teacher.class);
-		List<Teacher> teachers= jdbcTemplate.query(sql, rowMapper);
-		for (Teacher teacher : teachers) {//
-		    System.out.println(teacher.getName());
-		}
-		return teachers;
-	}
+@Mapper
+public interface TeacherRepository {
 	
-	public Teacher validTeacher(String account,String password) {
-		String sql="select teacher_id as id,teacher_account as account,teacher_name as name from t_teacher where teacher_account=? and teacher_password=md5(?)";		
-		//jdbc template
-		RowMapper<Teacher> rowMapper=new BeanPropertyRowMapper<Teacher>(Teacher.class);
-		List<Teacher> list = jdbcTemplate.query(sql, rowMapper,account,password);	
-		if (list != null && list.size()>0) {
-			System.out.println(list.get(0).getName());
-			return list.get(0);
-		}else
-			return null;				
-	}
+	@Results(id="teacherMap", value={ 
+    @Result(property = "id", column = "teacher_id",  jdbcType=JdbcType.INTEGER), 
+    @Result(property = "account", column = "teacher_account"),
+    @Result(property = "name", column = "teacher_name")
+	})
+
+	@Select("select * from t_teacher") 
+	public List<Teacher> findAll();
+	
+	@Select("select * from t_teacher where teacher_account=#{account} and teacher_password=md5(#{password})")
+	@ResultMap(value="teacherMap")
+	public Teacher validTeacher(@Param("account")String account,@Param("password")String password);
+	
+	@Select("select * from t_teacher where teacher_name like concat('%',#{name},'%')")
+	@ResultMap(value="teacherMap")			
+	public List<Teacher> findByName(@Param("name")String name);
+	
+	@Insert("INSERT INTO `lab`.`t_teacher` (`teacher_account`, `teacher_password`, `teacher_name`) VALUES (#{account}, md5(#{password}), #{name})")
+	@ResultMap(value="teacherMap")
+	public Boolean addTeacher(Teacher teacher);
 }
+//@Repository
+//public class TeacherRepository {	
+//	@Autowired
+//	private JdbcTemplate jdbcTemplate;	
+//	public List<Teacher> findAll(){		
+//		String sql="select teacher_id as id,teacher_account as account,teacher_name as name from t_teacher ";		
+//		RowMapper<Teacher> rowMapper=new BeanPropertyRowMapper<Teacher>(Teacher.class);
+//		List<Teacher> teachers= jdbcTemplate.query(sql, rowMapper);
+//		for (Teacher teacher : teachers) {//
+//		    System.out.println(teacher.getName());
+//		}
+//		return teachers;
+//	}
+//	
+//	public Teacher validTeacher(String account,String password) {
+//		String sql="select teacher_id as id,teacher_account as account,teacher_name as name from t_teacher where teacher_account=? and teacher_password=md5(?)";		
+//		//jdbc template
+//		RowMapper<Teacher> rowMapper=new BeanPropertyRowMapper<Teacher>(Teacher.class);
+//		List<Teacher> list = jdbcTemplate.query(sql, rowMapper,account,password);	
+//		if (list != null && list.size()>0) {
+//			System.out.println(list.get(0).getName());
+//			return list.get(0);
+//		}else
+//			return null;				
+//	}
+//}
